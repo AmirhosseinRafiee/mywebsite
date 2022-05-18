40ab5052b9519f63b6138a69753c30b01e5041a3
amirhr21 <amirhossein.rafiee.80@gmail.com>
2022-05-18 09:57:56 +0430

    added statics
    + templates fixed for statics

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'website'
 
 urlpatterns = [
-    path('', views.index_view),
-    path('contact', views.contact_view),
-    path('services', views.services_view),
+    path('', views.index_view, name='index'),
+    path('contact', views.contact_view, name='contact'),
+    path('services', views.services_view, name='services'),
 ]
